1378fd5205ffae10efecc42530cd04e96be2dfcd
11011 <jamesezekiel039@gmail.com>
2025-10-19 04:49:26 +0100

    settings: updated ALLOWED HOSTS and set debug to False

--- nosweat/settings.py
+++ nosweat/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default=[], cast=list)
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default=[], cast=lambda v: [s.strip() for s in v.split(",") if s])
 
 
 # Application definition
